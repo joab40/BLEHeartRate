e7ffd344d533dccb6fc21e5841a53174729e87b8
joab40 <tornvagen@gmail.com>
2026-01-03 23:31:37 +0100

    Initial Commit

---
+++ BLEHeartRate/BLEHeartRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEHeartRateApp.swift
+//  BLEHeartRate
+//
+//  Created by johan on 2026-01-03.
+//
+
+import SwiftUI
+
+@main
+struct BLEHeartRateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLEHeartRate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BLEHeartRate
+//
+//  Created by johan on 2026-01-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
